add7be497725f111a1b2474d97eb28f65667fa0b
GuPune <92207039+GuPune@users.noreply.github.com>
2022-06-07 15:30:09 +0700

    Update pdf.blade.php

--- resources/views/pdf.blade.php
+++ resources/views/pdf.blade.php
@@ -40,13 +40,12 @@
                 <div class="flex justify-center">
                     <img src="http://cmsecom2.idtest.work/img/1.png" alt="logo" width="100" height="100">
                 </div>
-                ชื่อ เว็ป / รึ ชื่อร้าน
-
+            
                 <div class="mt-8 bg-white dark:bg-gray-800 overflow-hidden shadow sm:rounded-lg">
                     <div class="grid grid-cols-1 md:grid-cols-6">
                         <div class="p-6">
                             <div class="flex items-center justify-center">
-                                <div class="ml-4 text-lg leading-7 font-semibold"><a href="https://laravel.com/docs" class="underline text-gray-900 dark:text-white">สาขา : </a></div>
+                                <div class="ml-4 text-lg leading-7">สาขา : </div>
                             </div>
                         </div>
                     </div>
