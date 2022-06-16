25621b5608ad03817ba567e6ff13718ff84d73b2
iboonkhet@gmail.com <iboonkhet@gmail.com>
2022-06-16 10:10:49 +0700

    Update index.blade.php

--- resources/views/page/consent/index.blade.php
+++ resources/views/page/consent/index.blade.php
@@ -29,7 +29,7 @@
         <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
             <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
                 <div class="flex justify-center">
-                    <img src="http://cmsecom2.idtest.work/img/1.png" alt="logo" width="100" height="100">
+                    <img src="http://consent.iddriver.com/img/1.png" alt="logo" width="100" height="100">
                 </div>
 
 
