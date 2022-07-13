a2ea8589f5965565a71eb2eb71d8de29babae51c
Rkknoob <iboonkhet@gmail.com>
2022-07-13 22:30:21 +0700

    Update register.blade.php

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -13,42 +13,37 @@
 
                         <input id="avatar" type="text" class="form-control" name="avatar" value="{{$avatar}}" required autocomplete="avatar" >
 
-                        <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
-
-                            <label for="name" class="col-md-4 control-label">Name</label>
+                        <div class="row mb-3">
+                            <label for="name" class="col-md-4 col-form-label text-md-end">{{ __('Name') }}</label>
 
                             <div class="col-md-6">
 
-                            @if(!empty($name))
+                           @if(!empty($name))
 
-                                <input id="name" type="text" class="form-control" name="name" value="{{$name}}" required autofocus>
+                           <input id="name" type="text" class="form-control" name="name" value="{{$name}}" required autofocus>
 
-                            @else
+                       @else
 
-                                <input id="name" type="text" class="form-control" name="name" value="{{ old('name') }}" required autofocus>
+                           <input id="name" type="text" class="form-control" name="name" value="{{ old('name') }}" required autofocus>
 
-                            @endif
+                       @endif
 
-                                @if ($errors->has('name'))
+                       @if ($errors->has('name'))
 
-                                    <span class="help-block">
+                       <span class="help-block">
 
-                                        <strong>{{ $errors->first('name') }}</strong>
+                           <strong>{{ $errors->first('name') }}</strong>
 
-                                    </span>
+                       </span>
 
-                                @endif
-
+                   @endif
                             </div>
-
                         </div>
 
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
+                        <div class="row mb-3">
+                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
 
                             <div class="col-md-6">
-
                                 @if(!empty($email))
 
                                 <input id="email" type="email" class="form-control" name="email" value="{{$email}}" required>
@@ -59,19 +54,19 @@
 
                                 @endif
 
-                                @if ($errors->has('email'))
 
-                                    <span class="help-block">
+                               @if ($errors->has('email'))
 
-                                        <strong>{{ $errors->first('email') }}</strong>
+                               <span class="help-block">
 
-                                    </span>
+                                   <strong>{{ $errors->first('email') }}</strong>
 
-                                @endif
+                               </span>
 
+                           @endif
                             </div>
-
                         </div>
+
                         <div class="row mb-3">
                             <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
 
