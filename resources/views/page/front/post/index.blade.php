1248e63be8cd767bfe527be8a3a2d9bfc4fd3239
Rkknoob <iboonkhet@gmail.com>
2022-07-20 14:25:49 +0700

    Update index.blade.php

--- resources/views/page/front/post/index.blade.php
+++ resources/views/page/front/post/index.blade.php
@@ -28,7 +28,10 @@
                         </div>
                     </div> <!---->
                     <img src="{{$post->images}}" class="rounded-xl">
-                    <img src="https://www.meditationphoto.com/images/approve.png" class="absolute bottom-4 right-4 w-10 h-10"></div> <div class="text-center">
+                    <img src="https://www.meditationphoto.com/images/approve.png" class="absolute bottom-4 right-4 w-10 h-10" style="
+                    width: 100%;">
+                    </div>
+                    <div class="text-center">
                         <a href="/photo/971744" class="">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" class="h-5 w-5 inline-block mr-1"><path stroke-linecap="round" stroke-linejoin="round" d="M13.828 10.172a4 4 0 00-5.656 0l-4 4a4 4 0 105.656 5.656l1.102-1.101m-.758-4.899a4 4 0 005.656 0l4-4a4 4 0 00-5.656-5.656l-1.1 1.1"></path></svg>
                             {{ $post->id }} </a>
