94ae058122ea436825c05c00a62b448896b7b3f0
Rkknoob <iboonkhet@gmail.com>
2022-07-20 12:58:15 +0700

    Update FileUploadController.php

--- app/Http/Controllers/FileUploadController.php
+++ app/Http/Controllers/FileUploadController.php
@@ -18,42 +18,28 @@ class FileUploadController extends Controller
 
 
 
+        $image = $request->file('file_upload');
+
+
+        $imageName = uniqid().'.'.$image->extension();
 
 
 
-        if($request->hasFile('file_upload'))
-        {
-            \Log::info('เข้าif');
-            //run actions with files
-            $files = $request->file('file_upload');
-
-            foreach($files as $key => $file)
-            {
-                \Log::info('เข้าloop');
-                //Action your file thing here!
-
-            }
-        }
+        $destinationPath = public_path('/storage/thumbnails');
 
 
 
+        $img = Image::make($image->path());
+        $img->resize(250, 250, function ($constraint) {
+            $constraint->aspectRatio();
+        })->save($destinationPath.'/'.$imageName);
 
-    //   $imageName = time().'.'.$image->extension();
-
-
-    //     $destinationPath = public_path('/storage/thumbnails');
-
-
-    //     $img = Image::make($image->path());
-    //     $img->resize(250, 250, function ($constraint) {
-    //         $constraint->aspectRatio();
-    //     })->save($destinationPath.'/'.$imageName);
 
 
 
 
         return response()->json([
-            'data' => 1
+            'data' => $imageName
         ], 200);
 
       //  return response()->json(['success'=>$input['imagename']]);
