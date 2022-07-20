0c7f59dd1cc31ca909b09f15657de546ac640e82
Rkknoob <iboonkhet@gmail.com>
2022-07-20 12:59:29 +0700

    Update index.blade.php

--- resources/views/page/front/upload/index.blade.php
+++ resources/views/page/front/upload/index.blade.php
@@ -65,7 +65,7 @@
         <form action="#" onsubmit=" return validateForm();" method="post" id="productForm" enctype="multipart/form-data" class="dropzone dz-clickable w-full rounded-xl">
             {!! csrf_field() !!}
             <div class="previews"></div>
-            <input type="text" name="multifile" id="multifile" value="">
+            <input type="hidden" name="multifile" id="multifile" value="">
             <div  id="uploadDropzone">
 
 
@@ -76,7 +76,7 @@
 
      @else
 
-     <div class="w-full lg:w-1/3 mx-auto p-5">
+     {{-- <div class="w-full lg:w-1/3 mx-auto p-5">
         <div class="dropzone dz-clickable w-full rounded-xl" id="uu">
 
             <div class="previews"></div>
@@ -91,10 +91,10 @@
 
         </div>
 
-            </div>
+            </div> --}}
 
 
-            {{-- <div class="w-full lg:w-1/3 mx-auto p-5">
+            <div class="w-full lg:w-1/3 mx-auto p-5">
                 <form action="#" onsubmit=" return validateForm();" method="post" id="productForm" enctype="multipart/form-data" class="dropzone dz-clickable w-full rounded-xl">
                     <div class="previews"></div>
                     <input type="hidden" name="multifile" id="multifile" value="">
@@ -104,7 +104,7 @@
                     <div data-dz-message class="dz-message" id="uplo">
                         <span>
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" class="h-6 w-6 mr-2 inline-block"><path stroke-linecap="round" stroke-linejoin="round" d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-8l-4-4m0 0L8 8m4-4v12"></path></svg>Upload ภาพกิจกรรม<br>(แตะเลือกภาพ หรือ ลากวาง)</span></div></form> <input type="file" multiple="multiple" tabindex="-1" class="dz-hidden-input hidden" style="\n        visibility: hidden;\n        position: absolute;\n        top: 0px;\n        left: 0px;\n        height: 0px;\n        width: 0px;\n      ">
-                    </div> --}}
+                    </div>
 
 
 
@@ -418,6 +418,8 @@ function login(vale)
 
                 init: function () {
                     this.on('success', function (file) {
+
+                        console.log(file);
                         var filename = JSON.parse(file.xhr.response);
                         arrImage.push(filename.data);
                         $('input[name=multifile]').val(arrImage);
