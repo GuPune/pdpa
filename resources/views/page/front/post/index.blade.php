25c75ded7646143453cbf3d74508c3f72f79efbc
iboonkhet@gmail.com <iboonkhet@gmail.com>
2022-07-25 16:18:05 +0700

    Update index.blade.php

--- resources/views/page/front/post/index.blade.php
+++ resources/views/page/front/post/index.blade.php
@@ -3,10 +3,12 @@
 @section('title','ศูนย์รวมภาพกิจกรรม')
 @section('description','เว็ปไซต์ศูนย์รวมภาพกิจกรรม ที่ใหญ่ที่สุดในโลก เพื่อสร้างสถิติ Guiness World Record')
 @section('keywords','นั่งสมาธิ,ภาพนั่งสมาธิ, ภาพวิธีการนั่งสมาธิ, ภาพการทำภาวนา, meditation, photo')
-
-@section('image','https://image.meditationphoto.com/uploads/large_S_1794431_c008bee93d.jpg')
-@section('imageurl','https://image.meditationphoto.com/uploads/large_S_1794431_c008bee93d.jpg')
-
+@foreach ($item['images'] as $key => $post)
+{{++$key}}
+@section('image',{{$post->images}})
+@section('imageurl',{{$post->images}})
+@break
+@endforeach
 @section('content')
 
 
@@ -25,8 +27,7 @@
                     2xl:grid-cols-3
                     gap-5
                   ">
-                  @foreach ($item['images'] as $key => $post)
-                  {{++$key}}
+                  @foreach ($item['images'] as $post)
                   <div class="relative row-span-2 cursor-pointer  modal-open text-center">
                       <div class="relative">
                           <div class="absolute left-1 top-1 text-center flex">
