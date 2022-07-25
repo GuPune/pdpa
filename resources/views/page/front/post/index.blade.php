24751629364aa76cd24f8e323bfa1bd47171a8b8
iboonkhet@gmail.com <iboonkhet@gmail.com>
2022-07-25 16:34:36 +0700

    Update index.blade.php

--- resources/views/page/front/post/index.blade.php
+++ resources/views/page/front/post/index.blade.php
@@ -4,7 +4,7 @@
 @section('description','เว็ปไซต์ศูนย์รวมภาพกิจกรรม ที่ใหญ่ที่สุดในโลก เพื่อสร้างสถิติ Guiness World Record')
 @section('keywords','นั่งสมาธิ,ภาพนั่งสมาธิ, ภาพวิธีการนั่งสมาธิ, ภาพการทำภาวนา, meditation, photo')
 @section('image','https://image.meditationphoto.com/uploads/large_S_1794431_c008bee93d.jpg')
-@section('imageurl','https://image.meditationphoto.com/uploads/large_S_1794431_c008bee93d.jpg')
+@section('imageurl','https://www.meditationphoto.com/photo/8757')
 @section('content')
 
 
