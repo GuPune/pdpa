5f87a7dae944d4c59d13d48eb60849d9012d21b9
iboonkhet@gmail.com <iboonkhet@gmail.com>
2022-07-25 13:53:40 +0700

    Update index.blade.php

--- resources/views/page/front/post/index.blade.php
+++ resources/views/page/front/post/index.blade.php
@@ -2,6 +2,12 @@
 
 
 @section('content')
+
+
+<meta property="og:title" content="The Rock" />
+<meta property="og:type" content="video.movie" />
+<meta property="og:url" content="https://www.imdb.com/title/tt0117500/" />
+<meta property="og:image" content="https://ia.media-imdb.com/images/rock.jpg" />
 <style>
 
 
