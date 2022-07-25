e0c5b30d478c50cc66bdf8b7b88f147d33f79b78
iboonkhet@gmail.com <iboonkhet@gmail.com>
2022-07-25 11:06:35 +0700

    Update LoginFormController.php

--- app/Http/Controllers/LoginFormController.php
+++ app/Http/Controllers/LoginFormController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class LoginFormController extends Controller
 {
@@ -12,6 +13,17 @@ class LoginFormController extends Controller
     public function showAdminLoginForm()
     {
 
-  return view('auth.login');
+
+        $admin = Auth::guard('admin')->user();
+
+
+if($admin){
+
+    return view('cms/home');
+}else {
+    return view('auth.login');
+
+}
+
     }
 }
